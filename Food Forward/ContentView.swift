96ce8f6f1f4e25d579f098fe29f4ae52159e8f66
nadolf <96559304+nadolf@users.noreply.github.com>
2024-09-23 23:36:47 -0400

    Initial Commit

---
+++ Food Forward/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food Forward
+//
+//  Created by Nadolf Tagoe on 9/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
